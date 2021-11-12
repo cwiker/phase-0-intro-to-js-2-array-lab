07676e0f2c6fe6f2d572da6338ba4a18921fe83c
Chris Wiker <cwiker7@gmail.com>
2021-11-12 00:16:43 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+
+cats.push("Ralph");
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+cats.unshift("Bob");
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+cats.pop([2]);
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+cats.shift([0]);
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function(cats) {
+    return (cats.push("Broom")); {
+    cats.concat();
+    }
+}
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function(cats) {
+    return (cats.unshift("Arnold")); {
+    cats.concat();
+    }
+}
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function(cats) {
+      return (cats.pop(2)); {
+        
+      }
+  }
